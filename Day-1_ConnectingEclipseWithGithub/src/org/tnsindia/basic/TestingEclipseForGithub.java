c78d749ca43388737ef2901806fef8414d64243c
pekha <pekha@LAPTOP-C38LJ0R3>
2022-09-26 21:42:33 +0530

    first commit

---
+++ Day-1_ConnectingEclipseWithGithub/src/org/tnsindia/basic/TestingEclipseForGithub.java
@@ -0,0 +1,10 @@
+package org.tnsindia.basic;
+
+public class TestingEclipseForGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("First Java Program");
+	}
+
+}
