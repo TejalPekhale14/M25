dc6bd31b8064320794c59662c2eaaa02817eb513
pekha <pekha@LAPTOP-C38LJ0R3>
2022-10-08 12:48:14 +0530

    commit

---
+++ AssignmentNo-2/src/org/tnsindia/strings/Example6.java
@@ -0,0 +1,13 @@
+package org.tnsindia.strings;
+
+import java.util.StringTokenizer;
+
+public class Example6 {
+
+	public static void main(String[] args) {
+		
+		StringTokenizer st=new StringTokenizer("Lets try this");
+        System.out.println(st.nextToken());
+	}
+
+}
